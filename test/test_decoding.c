fc70b524abd342260a3f97f34e0666ce52050b17
kolen <incredible.angst@gmail.com>
2019-01-08 03:24:26 +0300

    Add basic test

---
+++ test/test_decoding.c
@@ -0,0 +1,86 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "sds011.h"
+
+
+struct mock_device_receiver {
+  unsigned char received[10];
+};
+
+struct mock_device_sender {
+  unsigned char to_send[20];
+  int write_at;
+};
+
+void mock_device_sender_init(struct mock_device_sender *sender)
+{
+  sender->write_at = 0;
+}
+
+ssize_t mock_device_write(void *tx_device, const void *buf, size_t size)
+{
+  if (size != 10) {
+    fprintf(stderr, "Only writes of 10 bytes are supported\n");
+    abort();
+  }
+  memcpy(((struct mock_device_receiver*)tx_device)->received, buf, 10);
+  return size;
+}
+
+ssize_t mock_device_read(void *rx_device, void *buf, size_t size)
+{
+  struct mock_device_sender *reader = (struct mock_device_sender*)rx_device;
+  if (reader->write_at + size > 20) {
+    fprintf(stderr,
+	    "Trying to read too much data, pointer: %d, trying to write: %d",
+	    reader->write_at, (int)size);
+    abort();
+  }
+  memcpy(buf, &(reader->to_send[reader->write_at]), size);
+  reader->write_at += size;
+  return size;
+}
+
+void fill_checksum(struct mock_device_sender *sender) {
+  int i;
+  unsigned char sum = 0;
+  for(i=2; i<=7; i++) { sum += sender->to_send[i]; }
+  sender->to_send[8] = sum;
+}
+
+void test_parse_measurement(void **state) {
+  struct sds011_device_t device;
+  struct mock_device_sender sender;
+  struct mock_device_receiver receiver;
+  mock_device_sender_init(&sender);
+  sds011_init_with_read_write_fns(&device,
+				  mock_device_read,
+				  mock_device_write,
+				  (void*)&sender,
+				  (void*)&receiver);
+  static const char measurement[10] = {
+    0xaa, 0xc0,
+    0x34, 0x12, 0x78, 0x56,
+    0xfe, 0xca,
+    0x00, 0xab
+  };
+  memcpy(sender.to_send, measurement, 10);
+  fill_checksum(&sender);
+  struct sds011_reply_t reply;
+  sds011_read_reply(&device, &reply);
+  assert_int_equal(reply.type, sds011_reply_measurement);
+}
+
+int main(void)
+{
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_parse_measurement),
+  };
+
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
