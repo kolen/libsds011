a79fc06efb63595731ae259cd0963da91b811bef
kolen <incredible.angst@gmail.com>
2019-01-09 04:05:04 +0300

    Add test for waiting for header

--- test/test_decoding.c
+++ test/test_decoding.c
@@ -156,6 +156,31 @@ void test_parse_invalid_c5(void **state) {
   assert_true(result < 0);
 }
 
+void test_parse_wait_for_header(void **state) {
+  struct sds011_device_t device;
+  struct mock_device_sender sender;
+  mock_device_sender_init(&sender);
+  sds011_init_with_read_write_fns(&device,
+				  mock_device_read,
+				  mock_device_no_write,
+				  (void*)&sender,
+				  NULL);
+  static const unsigned char measurement[14] = {
+    0x01, 0xff, 0x12, 0x02,
+    0xaa, 0xc0,
+    0x34, 0x12, 0x78, 0x56,
+    0xfe, 0xca,
+    (unsigned char)(0x34 + 0x12 + 0x78 + 0x56 + 0xfe + 0xca),
+    0xab,
+  };
+  memcpy(sender.to_send, measurement, 14);
+  struct sds011_reply_t reply;
+  int result = sds011_read_reply(&device, &reply);
+  assert_return_code(result, 0);
+  assert_int_equal(reply.type, sds011_reply_measurement);
+  assert_int_equal(reply.measurement.pm2_5, 0x1234);
+}
+
 int main(void)
 {
   const struct CMUnitTest tests[] = {
@@ -163,6 +188,7 @@ int main(void)
     cmocka_unit_test(test_parse_data_reporting_mode),
     cmocka_unit_test(test_parse_invalid),
     cmocka_unit_test(test_parse_invalid_c5),
+    cmocka_unit_test(test_parse_wait_for_header),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
